b2182fb051e18da576c9457a6cd96836f1173d31
emiguelt <edwin.triana@bizagi.com>
2016-05-11 14:11:14 -0500

    cljmarathon ex 3-8

--- src/recursion.clj
+++ src/recursion.clj
@@ -6,22 +6,44 @@
     (* (first coll) (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (and (not (empty? coll)) (empty? (rest coll))))
 
 (defn my-last [coll]
-  :-)
+  (if (empty? coll)
+    nil
+    (let [tail (rest coll)]
+      (if (empty? tail)
+        (first coll)
+        (my-last tail)))))
 
 (defn max-element [a-seq]
-  :-)
+  (defn get-max [a b] (if (> a b) a b))
+  (defn inner-max [the-seq c-max]
+    (if (empty? the-seq)
+      c-max
+      (if (empty? (rest the-seq))
+        (get-max (first the-seq) c-max)
+        (inner-max (rest the-seq) (get-max (first the-seq) c-max)))))
+  (if (empty? a-seq)
+    nil
+    (inner-max (rest a-seq) (first a-seq))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2))
+    seq-1
+    seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    nil
+    (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (cons (first a-seq) (my-filter pred? (rest a-seq)))
+      (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
   :-)
