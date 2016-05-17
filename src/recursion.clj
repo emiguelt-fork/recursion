cae76c9b3305eb25f50d2592da459593fb8e0aef
emiguelt <edwin.triana@bizagi.com>
2016-05-17 14:21:26 -0500

    cljmarathon ex 12-17

--- src/recursion.clj
+++ src/recursion.clj
@@ -64,25 +64,43 @@
     :else a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+    (not (= (count a-seq) (count b-seq))) false
+    (and (empty? a-seq) (empty? b-seq)) true
+    (not (= (first a-seq) (first b-seq))) false
+    :else (seq= (rest a-seq) (rest b-seq))))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (cond
+    (or (empty? seq-1) (empty? seq-2)) '()
+    (empty? seq-2) seq-2
+    :else (cons (f (first seq-1) (first seq-2)) (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
-  :-)
+  (if (= 0 k) 
+    1
+    (* n (power n (- k 1)))))
 
 (defn fib [n]
-  :-)
+  (cond
+    (= 0 n) 0
+    (= 1 n) 1
+    :else (+ (fib (- n 1)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (if (< how-many-times 1) 
+    '()
+    (cons what-to-repeat (my-repeat (- how-many-times 1) what-to-repeat))))
+
 
 (defn my-range [up-to]
-  [:-])
+  (if (= 0 up-to) 
+    '()
+    (cons (- up-to 1) (my-range (- up-to 1)))))
 
 (defn tails [a-seq]
-  [:-])
+  (if (empty? a-seq) '([])
+    (cons a-seq (tails (rest a-seq)))))
 
 (defn inits [a-seq]
   [:-])
